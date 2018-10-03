8e117fdf326dd2e36387888a0d62408954a4b56c
danilo.meneghel <danilo.meneghel@gmail.com>
2018-10-03 14:56:37 -0300

    Update files

--- books/models.py
+++ books/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Book(models.Model):
     name = models.CharField(max_length=60)
+    author = models.CharField(max_length=60)
     price = models.DecimalField(max_digits=9, decimal_places=2)
     description = models.TextField(blank=True)
 
